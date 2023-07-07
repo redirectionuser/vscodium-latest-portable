a140eaecfbf36582debb529815370574b22a058f
Redirection User <redirection.user@outlook.com>
2023-07-07 14:44:35 +0200

    fixed possibly dangerous operations in userprofile-relocation.bat

--- Other/Source/userprofile-relocation.bat
+++ Other/Source/userprofile-relocation.bat
@@ -7,14 +7,14 @@ if not exist "%~dp0..\..\..\CommonFiles\UserProfile\AppData\Roaming\Microsoft\Wi
 )
 
 if exist "%~dp0..\..\..\CommonFiles\UserProfile\AppData\Roaming\Microsoft\Windows\Recent" (
-  del /q "%~dp0..\..\..\CommonFiles\UserProfile\AppData\Roaming\Microsoft\Windows\Recent"
+  rmdir /q "%~dp0..\..\..\CommonFiles\UserProfile\AppData\Roaming\Microsoft\Windows\Recent"
   mklink /J "%~dp0..\..\..\CommonFiles\UserProfile\AppData\Roaming\Microsoft\Windows\Recent" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Recent" 1>NUL 2>&1
 )
 
 cd /d "%~dp0..\..\..\CommonFiles\UserProfile"
 for /f "tokens=2 delims=\" %%i in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" ^| findstr "REG_EXPAND_SZ" ^| findstr /V "AppData"') ^
 do (
-  del /q "%%~i"
+  rmdir /q "%%~i"
   mklink /J "%%~i" "%USERPROFILE%\%%~i" 1>NUL 2>&1
 )
 
